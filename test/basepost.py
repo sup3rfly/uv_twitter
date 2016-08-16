c64c36423fb050e769b0715b207c6b8ea4173749
Alexandru Georgescu <alexandru.c.georgescu@intel.com>
2016-08-16 14:24:08 +0300

    Removed authentication lines from basepost and cursor

--- test/basepost.py
+++ test/basepost.py
@@ -1,23 +1,11 @@
 import time, datetime, string, random
 import tweepy
+from auth import *
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 
 
-#UberVU tokens
-ckey = 'eVP3L5l1vX6Q20qEWWJBelIfV'
-csecret = '27VEuF4qErAGCishfleG5zLvBJ1AAmJ3IoVdYJ75xQQxxga9UG'
-atoken = '3575504721-dIpxGbqa4crzDWhwKo16dV56L4lyYHHgi0qczhx'
-asecret = 'IzalISFtV5JLLz7XszaFz8ajUPPZhaGMIoG86aTAuIjEC'
-
-# Authentication
-auth = tweepy.OAuthHandler(ckey, csecret)
-auth.set_access_token(atoken, asecret)
-auth.secure = True
-api=tweepy.API(auth)
-
-usertest = api.get_user(screen_name = '@test_uv')
 
 #function that creates a random id needed to have unique tweets
 #TODO create custom rand function
--- test/cursor.py
+++ test/cursor.py
@@ -1,17 +1,6 @@
 import tweepy
+from auth import *
 from tweepy import Cursor
-
-#UberVU tokens
-ckey = 'eVP3L5l1vX6Q20qEWWJBelIfV'
-csecret = '27VEuF4qErAGCishfleG5zLvBJ1AAmJ3IoVdYJ75xQQxxga9UG'
-atoken = '3575504721-dIpxGbqa4crzDWhwKo16dV56L4lyYHHgi0qczhx'
-asecret = 'IzalISFtV5JLLz7XszaFz8ajUPPZhaGMIoG86aTAuIjEC'
-
-auth = tweepy.OAuthHandler(ckey, csecret)
-auth.set_access_token(atoken, asecret)
-auth.secure = True
-api=tweepy.API(auth)
-
 
 def search_status(query, lang=None, locale=None, count=None, until=None, items=None):
     response_list=[]
