ea115e1aa070f6fea13ca483086d9993b419196f
Alex Georgescu <alexandru.c.georgescu@gmail.com>
2016-08-16 14:29:53 +0300

    Cursor cleanup

--- test/cursor.py
+++ test/cursor.py
@@ -1,17 +1,12 @@
-import tweepy
 from auth import *
 from tweepy import Cursor
 
+#This function uses the api.search from Cursor tweepy class to retreive tweets. It receives query as mandatory parameter
+# and lang, locale, count, until, items as optional
 def search_status(query, lang=None, locale=None, count=None, until=None, items=None):
     response_list=[]
 
-    # print "Searching ", query
     for status in Cursor(api.search, q=query, lang=lang, locale=locale, count=count, until=until).items(items):
-        # print "Entered for loop"
-        # print status.text.encode('utf-8')
-        # print "Type:", status.text
         response_list.append(status)
-#    print response_list
-    return response_list
 
-# search_status("Test", 763391457952567296)
+    return response_list
